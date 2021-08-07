61c1ac2fa439c74c0967d43e6ab764cfaab7a6c0
udyapatel <udya.patel2050@gmail.com>
2021-08-07 12:13:29 +0530

    4 files modified, 9 added, 7 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { CarouselComponent } from './carousel/carousel.component';
 
+
 @NgModule({
   imports:      [
      BrowserModule,
-     FormsModule
+     FormsModule,
+     
+     BrowserAnimationsModule
      ],
   declarations: [ 
      AppComponent,
@@ -19,6 +24,7 @@ import { CarouselComponent } from './carousel/carousel.component';
      HeaderComponent,
      HomeComponent,
      CarouselComponent
+   
     
     ],
   bootstrap:    [ AppComponent ]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,4 +16,5 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+
 }
