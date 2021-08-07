ad3182eba5d3a522b23a65a4259adcaa47605fe5
udyapatel <udya.patel2050@gmail.com>
2021-08-07 13:09:08 +0530

    1 file modified

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -12,7 +12,7 @@ export class CarouselComponent implements OnInit {
 
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
-    config.interval = 2000;
+    config.interval = 8000;
     config.keyboard = true;
     config.pauseOnHover = true;
   }
